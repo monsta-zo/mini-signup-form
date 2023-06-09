f8116b888536172a7c3e661bc26876eefb4b3a52
Junseo <monstazo911@gmail.com>
2023-06-09 10:38:58 +0900

    answer

--- src/index.js
+++ src/index.js
@@ -1,121 +1,122 @@
-// TODO: 이 곳에 정답 코드를 작성해주세요.
+// 1.  autofocus 구현
+const $id = document.getElementById('id')
+const $idMsg = document.getElementById('id-msg')
+window.addEventListener('load', () => $id.focus())
 
-const idInput = document.getElementById('id')
-const pwInput = document.getElementById('pw')
-const pwCheckInput = document.getElementById('pw-check')
-const submit = document.getElementById('submit')
-const modal = document.getElementById('modal')
-const cancelBtn = document.getElementById('cancel-btn')
-const approveBtn = document.getElementById('approve-btn')
-const increaseBtn = document.getElementById('increase-font-btn')
-const decreaseBtn = document.getElementById('decrease-font-btn')
+// 2. 유효성 검사 로직 구현
+const $pw = document.getElementById('pw')
+const $pwMsg = document.getElementById('pw-msg')
 
-let fontSize = 16
+const $pwCheck = document.getElementById('pw-check')
+const $pwCheckMsg = document.getElementById('pw-check-msg')
 
-document.addEventListener('DOMContentLoaded', () => {
-    // autofocus
-    idInput.focus()
-})
+const ID_REGEX = new RegExp('^[a-z0-9_-]{5,20}$')
+const PW_REGEX = new RegExp('^[a-zA-Z0-9]{8,16}$')
 
-idInput.addEventListener('focusout', valId)
-pwInput.addEventListener('focusout', valPw)
-pwCheckInput.addEventListener('focusout', valPwCheck)
-
-submit.addEventListener('click', (event) => {
-    event.preventDefault()
-    const trueId = valId()
-    const truePw = valPw()
-    const truePwCheck = valPwCheck()
-    if (trueId && truePw && truePwCheck) {
-        const id = document.getElementById('confirm-id')
-        const pw = document.getElementById('confirm-pw')
-        modal.showModal()
-        id.textContent = idInput.value
-        pw.textContent = pwInput.value
-    }
-})
-
-cancelBtn.addEventListener('click', () => {
-    modal.close()
-})
-
-approveBtn.addEventListener('click', () => {
-    window.alert('가입되었습니다 🥳')
-    modal.close()
-})
-
-increaseBtn.addEventListener('click', (event) => {
-    fontSize++
-    document.documentElement.style.fontSize = `${fontSize}px`
-    if (fontSize !== 12) {
-        decreaseBtn.disabled = false
-    }
-    if (fontSize === 20) {
-        event.target.disabled = true
-    }
-})
-
-decreaseBtn.addEventListener('click', (event) => {
-    fontSize--
-    document.documentElement.style.fontSize = `${fontSize}px`
-    if (fontSize !== 20) {
-        increaseBtn.disabled = false
-    }
-    if (fontSize === 12) {
-        event.target.disabled = true
-    }
-})
-
-// ID 유효성 검사
-function valId() {
-    const id = idInput.value
-    const idMsg = document.getElementById('id-msg')
-    const regex = /^[a-z0-9_-]{5,20}$/
-    if (!id) {
-        idMsg.textContent = '필수 정보입니다.'
-    } else if (!regex.test(id)) {
-        idMsg.textContent =
-            '5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.'
-    } else {
-        idMsg.textContent = ''
-        idInput.style.borderColor = 'white'
-        return true
-    }
-    idInput.style.borderColor = 'red'
-    return false
+const ERROR_MSG = {
+    required: '필수 정보입니다.',
+    invalidId:
+        '5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.',
+    invalidPw: '8~16자 영문 대 소문자, 숫자를 사용하세요.',
+    invalidPwCheck: '비밀번호가 일치하지 않습니다.',
 }
 
-// 비밀번호 유효성 검사
-function valPw() {
-    const pw = pwInput.value
-    const pwMsg = document.getElementById('pw-msg')
-    const regex = /^[A-z0-9]{8,16}$/
-    if (!pw) {
-        pwMsg.textContent = '필수 정보입니다.'
-    } else if (!regex.test(pw)) {
-        pwMsg.textContent = '8~16자 영문 대 소문자, 숫자를 사용하세요.'
+// 3. 커스텀 에러 메시지
+const checkRegex = (target) => {
+    const { value, id } = target // destructuring 구조분해할당
+    // const value = target.value; const id = target.id
+    if (value.length === 0) {
+        return 'required'
     } else {
-        pwMsg.textContent = ''
-        pwInput.style.borderColor = 'white'
-        return true
+        switch (id) {
+            case 'id':
+                return ID_REGEX.test(value) ? true : 'invalidId'
+            case 'pw':
+                return PW_REGEX.test(value) ? true : 'invalidPw'
+            case 'pw-check':
+                return value === $pw.value ? true : 'invalidPwCheck'
+        }
     }
-    pwInput.style.borderColor = 'red'
-    return false
 }
 
-// 비밀번호 확인 유효성 검사
-function valPwCheck() {
-    const pwCheck = pwCheckInput.value
-    const pwCheckMsg = document.getElementById('pw-check-msg')
-    if (!pwCheck) {
-        pwCheckMsg.textContent = '필수 정보입니다.'
-    } else if (pwCheck !== pwInput.value) {
-        pwCheckMsg.textContent = '비밀번호가 일치하지 않습니다.'
+const checkValidation = (target, msgTarget) => {
+    const isValid = checkRegex(target)
+    if (isValid !== true) {
+        target.classList.add('border-red-600')
+        msgTarget.innerText = ERROR_MSG[isValid]
     } else {
-        pwCheckMsg.textContent = ''
-        pwCheckInput.style.borderColor = 'white'
-        return true
+        target.classList.remove('border-red-600')
+        msgTarget.innerText = ''
     }
-    pwCheckInput.style.borderColor = 'red'
-    return false
+    return isValid
+}
+
+$id.addEventListener('focusout', () => checkValidation($id, $idMsg))
+
+$pw.addEventListener('focusout', () => checkValidation($pw, $pwMsg))
+
+$pwCheck.addEventListener('focusout', () =>
+    checkValidation($pwCheck, $pwCheckMsg)
+)
+
+// 4. 입력 확인 모달 창
+const $submit = document.getElementById('submit')
+const $modal = document.getElementById('modal')
+
+const $confirmId = document.getElementById('confirm-id')
+const $confirmPw = document.getElementById('confirm-pw')
+
+const $cancelBtn = document.getElementById('cancel-btn')
+const $approveBtn = document.getElementById('approve-btn')
+
+$submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    const isValidForm =
+        checkValidation($id, $idMsg) === true &&
+        checkValidation($pw, $pwMsg) === true &&
+        checkValidation($pwCheck, $pwCheck) === true
+
+    if (isValidForm) {
+        $confirmId.innerText = $id.value
+        $confirmPw.innerText = $pw.value
+        $modal.showModal()
+    }
+})
+
+$cancelBtn.addEventListener('click', () => {
+    $modal.close()
+})
+
+$approveBtn.addEventListener('click', () => {
+    window.alert('가입되었습니다 🥳 ')
+    $modal.close()
+})
+
+// 5. 폰트 사이즈 조절 버튼
+const $increaseFontBtn = document.getElementById('increase-font-btn')
+const $decreaseFontBtn = document.getElementById('decrease-font-btn')
+
+const $html = document.documentElement
+
+const MAX_FONT_SIZE = 20
+const MIN_FONT_SIZE = 12
+
+const getHtmlFontSize = () => {
+    return parseFloat(window.getComputedStyle($html).fontSize)
+}
+
+$increaseFontBtn.addEventListener('click', () => {
+    onClickFontSizeControl('increase')
+})
+
+$decreaseFontBtn.addEventListener('click', () => {
+    onClickFontSizeControl('decrease')
+})
+
+const onClickFontSizeControl = (flag) => {
+    const fontSize = getHtmlFontSize()
+    let newFontSize = flag === 'increase' ? fontSize + 1 : fontSize - 1
+    $html.style.fontSize = newFontSize
+    $decreaseFontBtn.disabled = newFontSize <= MIN_FONT_SIZE
+    $increaseFontBtn.disabled = newFontSize >= MAX_FONT_SIZE
 }
